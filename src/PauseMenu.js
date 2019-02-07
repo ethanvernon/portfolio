17aa602549329ea6d760589d3147e3a4aee50c9d
Ethan Vernon <ethan.vernon@colorado.edu>
2019-02-07 12:15:02 +0800

    thumbnails to portfolio page, zeroed out transitions

--- src/PauseMenu.js
+++ src/PauseMenu.js
@@ -6,6 +6,12 @@ import {Contact} from './Contact';
 import {Blog} from './Blog';
 import {Title} from './Title';
 import {Back} from './Back';
+import spell from "./spell-selector.png";
+import dice from "./story-dice.png";
+import npc from "./npc-gen.png";
+import pomo from "./pomodoro-clock.png";
+import ab from "./ab-workout.png";
+import wild from "./wildshape-calc.png";
 
 
 export class PauseMenu extends Component {
@@ -14,15 +20,48 @@ export class PauseMenu extends Component {
 	    super(props);
 
 	    this.state = {
-			page: 'home'
+			page: 'home',
+			thumbs: [spell, dice, npc, pomo, ab, wild],
+			currentThumb: dice
 	    };
 
 		this.handleMenuClick = this.handleMenuClick.bind(this);
+		this.handleArrowClick= this.handleArrowClick.bind(this);
 	}
 
 	handleMenuClick(pageName) {		
 		this.setState({
 			page: pageName,
+		})
+	}
+
+	handleArrowClick(direction) {
+		//save array and declare newThumb
+		let arr = this.state.thumbs;
+		let newThumb;
+
+		//fine index of current thumbnail
+		let current = arr.findIndex((current) => {return current == this.state.currentThumb});
+		
+		//if direction is left/right from start/end
+		if (direction==='left' && current==0) {
+			newThumb=arr.length-1;
+		}
+		else if (direction==='right' && current==arr.length-1) {
+			newThumb=0;
+		}
+		//if direction is left
+		else if (direction==='left') {
+			newThumb=current-1;
+		}
+		//if direction is right
+		else if (direction==='right') {
+			newThumb=current+1;
+		}
+
+		//update state for new thumb
+		this.setState({
+			currentThumb: arr[newThumb]
 		})
 	}
 
@@ -38,7 +77,9 @@ export class PauseMenu extends Component {
 						page={this.state.page}/>
 					<Portfolio
 						handleClick={this.handleMenuClick}
-						page={this.state.page}/>
+						page={this.state.page}
+						thumb={this.state.currentThumb}
+						handleArrowClick={this.handleArrowClick}/>
 					<Resume
 						handleClick={this.handleMenuClick}
 						page={this.state.page}/>
--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
-import spell from "./spell-selector.png";
 
 export class Portfolio extends Component {
 	constructor(props) {
 		super(props);
 
 		this.handleClick = this.handleClick.bind(this);
+		this.handleArrowClick = this.handleArrowClick.bind(this);
 	}
 
 	handleClick() {
 		this.props.handleClick('portfolio');
 	}
 
+	handleArrowClick(direction) {
+		this.props.handleArrowClick(direction);
+	}
+
 	render() {
 
+		//declare var to use as className
 		let hidden;
 
+		//check current page for appropriate classes
 		if (this.props.page === 'portfolio') {
 			hidden='fadeInOut current';
 		} else if (this.props.page === 'home') {
@@ -29,11 +35,11 @@ export class Portfolio extends Component {
 				<span>Portfolio</span>
 				{this.props.page==='portfolio' &&
 					<div className='frame'>
-						<span className='thumbnail-container'></span><img className='port-thumb' src={spell}/>
-						<div id='left-arrow' className='arrow'>
+						<span className='thumbnail-container'></span><img className='port-thumb' src={this.props.thumb}/>
+						<div id='left-arrow' className='arrow' onClick={() => this.handleArrowClick('left')}>
 							&#171;
 						</div>
-						<div id='right-arrow' className='arrow'>
+						<div id='right-arrow' className='arrow' onClick={() => this.handleArrowClick('right')}>
 							&#187;
 						</div>
 					</div>
